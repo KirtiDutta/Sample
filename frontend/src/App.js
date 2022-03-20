ee6e3bcc5adedf456be0b84500acd4fa4a143a51
Kirti Dutta <kirtidutta247@gmail.com>
2022-03-20 13:24:13 +0530

    frontend completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 const App = () => {
   return (
     <>
-      <h1>Welcome to BookZone</h1>
-    </>
-  );
+    <Header />
+    <Container>
+    <main className='py-4'>
+    <Router>
+    <Routes>
+      <Route path='/' element={<HomeScreen />} />
+      <Route path='/product/:id' element={<ProductScreen />} /> 
+    </Routes>
+   </Router>
+    </main>
+    </Container>
+  <Footer />
+  </>
+);
 }
 
 export default App;
---
+++ frontend/src/products.js
@@ -0,0 +1,77 @@
+const products = [
+    {
+        _id: '1',
+        name: 'book1',
+        image: '/images/book1.jpg',
+        description: 
+        'The Song of Achilles is a 2011 novel by American writer Madeline Miller. Set during the Greek Heroic Age, it is an adaptation of Homer Iliad as told from the perspective of Patroclus.',
+        condition: 'New',
+        price: '200',
+        countInStock: '5',
+        rating: '4.5',
+        numReviews: '4',
+
+    },
+    {
+        _id: '2',
+        name: 'book2',
+        image: '/images/book2.jpg',
+        description: 
+        'It Ends With Us is one of the most powerful books of 2016 and one of the most raw, honest, inspiring, and profoundly beautiful stories I have ever read. It was not anything like what I expected, but it delivered so much more than I ever hoped for.',
+        condition: 'New',
+        price: '210',
+        countInStock: '20',
+        rating: '4',
+        numReviews: '3',
+    },
+    {
+        _id: '3',
+        name: 'book3',
+        image: '/images/book3.jpg',
+        description: 
+        'The Stories We Never Tell is a contemporary story that not only revolves around mental health issues, but also the pressures that young people put on themselves.',
+        condition: 'New',
+        price: '220',
+        countInStock: '10',
+        rating: '4.5',
+        numReviews: '4',
+    },
+    {
+        _id: '4',
+        name: 'book4',
+        image: '/images/book4.jpg',
+        description: 
+        'Based on the acclaimed novel by Joshua Henkin, The World Without You deals with a grieving family that gathers on the Fourth of July weekend to honor the memory of their journalist son, Leo Frankel, who was killed in Iraq the previous year.',
+        condition: 'Readable',
+        price: '90',
+        countInStock: '5',
+        rating: '4.5',
+        numReviews: '4',
+    },
+    {
+        _id: '5',
+        name: 'book5',
+        image: '/images/book5.png',
+        description: 
+        'Sometimes pretending to fall in love can be as difficult as actually doing so! At least that is the concept of Alex Light is new book, The Upside of Falling.',
+        condition: 'Mostly New',
+        price: '120',
+        countInStock: '6',
+        rating: '4.5',
+        numReviews: '4',
+    },
+    {
+        _id: '6',
+        name: 'book6',
+        image: '/images/book6.jpg',
+        description: 
+        ' Ugly Love is an emotional exploration of a woman fighting her attraction for a man determined not to fall in love with her, but she can not seem to understand why he keeps her at arm length.',
+        condition: 'Mostly New',
+        price: '130',
+        countInStock: '3',
+        rating: '4.5',
+        numReviews: '4',
+    }
+]
+
+export default products
